57f58380101df6216eaa94906c54ca4accaa277d
tigocode <tiago.dantas16@live.com>
2024-08-08 22:31:49 -0300

    segunda versao do codigo: estilizacao do código para exibir o efeito do dark style

--- src/app/page.js
+++ src/app/page.js
@@ -4,25 +4,32 @@ import { BsSun, BsMoon } from "react-icons/bs";
 import styles from './page.module.css';
 
 export default function Home() {
-  const [ alterarTema, setAlterarTema ] = useState(false);
+  const [alterarTema, setAlterarTema] = useState(false);
 
   function trocarTemaPagina() {
     setAlterarTema(!alterarTema);
   }
 
   return (
-    <>
-      <header>
+    < div className={alterarTema ? styles.dark_mode : styles.light_mode}>
+      <header className={styles.header_container}>
         <h1>Coffe Shop</h1>
         <button onClick={trocarTemaPagina}>
-          { alterarTema ? <BsSun /> : <BsMoon />}
+          {alterarTema ? <BsSun /> : <BsMoon />}
         </button>
       </header>
-      <main className={styles.main}>
-        <h2></h2>
-        <p></p>
-        <button></button>
+      <main className={styles.main_container}>
+        <div>
+          <h2>A melhor cafeteria do Brasil</h2>
+          <p>
+            Venha conhecer a melhor cafeteria do Brasil!
+            Localizada no coração de São Paulo, oferecemos cafés artesanais de alta qualidade e um ambiente acolhedor.
+            Desfrute de nossas bebidas exclusivas e deliciosos acompanhamentos enquanto relaxa em um espaço único.
+            Esperamos por você para uma experiência inesquecível!
+          </p>
+          <button>Nossas Lojas</button>
+        </div>
       </main>
-    </>
+    </div>
   );
 }
